80aacfeb43f80747bc3e0f5c1a51b0985accbabe
mahendar karnati <mahendar.karnati@gmail.com>
2025-08-05 19:45:18 +0530

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  const baseURL = process.env.REACT_APP_API_URL;
+
   withCredentials: true, // Needed only if using cookies; optional here
   headers: {
     "Content-Type": "application/json"
